2af49a8e72cf4395cff5ae86a3742d15139827d0
trunishka <trunishka.sergey@gmail.com>
2016-11-05 21:27:22 +0200

    small part of Homework

---
+++ python 1 homework/min_max.py
@@ -0,0 +1,5 @@
+__author__ = 'Trunishka'
+numbers = [1, 2, 3, 4, 5, 12, 123, 43, 67, 213, 9, 5, 472]
+x = min(numbers)
+y = max(numbers)
+print("max =", x, "min =", y)
---
+++ python 1 homework/multiplication table.py
@@ -0,0 +1,6 @@
+__author__ = 'Trunishka'
+
+for i in range(2, 11):
+    for x in range(2, 11):
+        print(i,"*", x,"=", i * x)
+print("Finish")
