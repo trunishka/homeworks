ca72dbe3eaa23b91072ae3d041d46548ad95ed1a
trunishka <trunishka.sergey@gmail.com>
2016-11-06 13:49:30 +0200

    additional correction of negative munbers and a ledder from munbers file

--- python 1 homework/ledder form numbers.py
+++ python 1 homework/ledder form numbers.py
@@ -4,8 +4,9 @@ user_in = input("insert the number of stairs")
 try:
     number = int(user_in)
     if number > 0:
-        for i in range(number):
-            print(i * "*")
+        for i in range(number + 1):
+            x.append(i)
+            print(x)
     else:
         print("the number must be more than zero")
 except ValueError:
