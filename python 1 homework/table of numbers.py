6e94dd507d5dfe044f8bcf9cf3edd314d1881b63
trunishka <trunishka.sergey@gmail.com>
2016-11-07 22:32:51 +0200

    add a variant with i-iteration

--- python 1 homework/table of numbers.py
+++ python 1 homework/table of numbers.py
@@ -1,9 +1,16 @@
 b = "1 2 3\n4 5 6 \n7 8 9"
 print(b)
+
 z = [[1,2,3],[4,5,6],[7,8,9]]
-a = z[0]
-c = z[1]
-d = z[2]
-print(a)
-print(c)
-print(d)
+for i in z:
+    print(i)
+# a = z[0]
+# c = z[1]
+# d = z[2]
+# print(a)
+# print(c)
+# print(d)
+
+e = ("1 2 3", "4 5 6", "7 8 9")
+for i in e:
+    print(i)
